f1adf261a0af9632fb85a9f01f687516c7bcdc5b
noman081 <abdullahalnoman642@gmail.com>
2022-03-16 19:54:19 +0600

    After adding some users

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LoadUsers></LoadUsers>
     </div>
   );
+}
+
+function LoadUsers() {
+
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch('https://randomuser.me/api/?results=10')
+      .then(res => res.json())
+      .then(data => setUsers(data.results));
+  }, [])
+  // console.log(users);
+  return (
+    <div>
+      <h1>Load Some users</h1>
+      {
+        users.map(user => <User imgUrl={user.picture.large} fname={user.name.first} lname={user.name.last} email={user.email} phone={user.phone}></User>)
+      }
+    </div>
+  )
+
+}
+
+function User(props) {
+  return (
+    <div className='user'>
+      <img src={props.imgUrl} alt="" />
+      <h3>Name: {props.fname} {props.lname}</h3>
+      <p>Email: {props.email}</p>
+      <p>Phone: {props.phone}</p>
+    </div>
+  )
 }
 
 export default App;
